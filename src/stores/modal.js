cfa4b434659badf05ad765724dc683b83482be7f
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-07 11:29:43 +0300

    Trying Setup Stores for modal store

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,7 +1,14 @@
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
-export default defineStore("modal", {
-  state: () => ({
-    isOpen: false,
-  }),
+// export default defineStore("modal", {
+//   state: () => ({
+//     isOpen: false,
+//   }),
+// });
+
+export const useModalStore = defineStore("modal", () => {
+  const isOpen = ref(false);
+
+  return { isOpen };
 });
