9f206c060372a7ca5d1893d8a2a7704bccb55bf2
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-07 11:31:37 +0300

    Reverting previous commit

--- src/stores/modal.js
+++ src/stores/modal.js
@@ -1,14 +1,7 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
 
-// export default defineStore("modal", {
-//   state: () => ({
-//     isOpen: false,
-//   }),
-// });
-
-export const useModalStore = defineStore("modal", () => {
-  const isOpen = ref(false);
-
-  return { isOpen };
+export default defineStore("modal", {
+  state: () => ({
+    isOpen: false,
+  }),
 });
