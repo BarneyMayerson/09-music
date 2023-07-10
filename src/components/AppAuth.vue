56a8fc0bd26e5bf269ff032810997e8066162839
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-10 11:06:19 +0300

    10-005 Applying Rules

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -83,7 +83,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <VeeForm v-show="tab === 'register'">
+          <VeeForm v-show="tab === 'register'" :validation-schema="schema">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -93,6 +93,7 @@
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
                 placeholder="Enter Name"
               />
+              <ErrorMessage name="name" class="text-red-600 text-xs" />
             </div>
             <!-- Email -->
             <div class="mb-3">
@@ -168,6 +169,14 @@ export default {
   data() {
     return {
       tab: "login",
+      schema: {
+        name: "required",
+        email: "",
+        password: "",
+        confirm_password: "",
+        country: "",
+        tos: "",
+      },
     };
   },
 
