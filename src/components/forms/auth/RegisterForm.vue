08af899e0aa22f6af135318a3a0822a1acee80cc
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-13 15:51:17 +0300

    11-010 Logging the user in after Signup

--- src/components/forms/auth/RegisterForm.vue
+++ src/components/forms/auth/RegisterForm.vue
@@ -120,6 +120,8 @@
 
 <script>
 import { auth, usersCollection } from "@/includes/firebase";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
   name: "RegisterForm",
@@ -146,6 +148,10 @@ export default {
       reg_alert_variant: "bg-blue-500",
       reg_alert_msg: "Please wait! Your account is being created.",
     };
+  },
+
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
   },
 
   methods: {
@@ -180,6 +186,8 @@ export default {
         this.reg_alert_msg = "An unexpected error occured. Please try again later.";
         return;
       }
+
+      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
