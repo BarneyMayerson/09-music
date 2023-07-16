84bb937f19e926de5308bb5bd18e376b6651239e
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-16 08:45:31 +0300

    11-015 Persisting the User Authentication

--- src/App.vue
+++ src/App.vue
@@ -268,6 +268,9 @@
 </template>
 
 <script>
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { auth } from "@/includes/firebase";
 import AppHeader from "@/components/AppHeader.vue";
 import AppAuth from "@/components/AppAuth.vue";
 
@@ -275,5 +278,15 @@ export default {
   name: "App",
 
   components: { AppHeader, AppAuth },
+
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+
+  created() {
+    if (auth.currentUser) {
+      this.userLoggedIn = true;
+    }
+  },
 };
 </script>
