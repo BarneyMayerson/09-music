6d242c9e8aa01bc4277bdbedf1398b4d8c0d5b02
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-16 08:25:04 +0300

    11-013 Connecting the User with their Data

--- src/stores/user.js
+++ src/stores/user.js
@@ -8,14 +8,21 @@ export default defineStore("user", {
 
   actions: {
     async register(values) {
-      await auth.createUserWithEmailAndPassword(values.email, values.password);
+      const userCredenlials = await auth.createUserWithEmailAndPassword(
+        values.email,
+        values.password
+      );
 
-      await usersCollection.add({
+      await usersCollection.doc(userCredenlials.user.uid).set({
         name: values.name,
         email: values.email,
         country: values.country,
         age: values.age,
         attitude: values.attitude,
+      });
+
+      await userCredenlials.user.updateProfile({
+        displayName: values.name,
       });
 
       this.userLoggedIn = true;
