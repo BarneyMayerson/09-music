77d2883309860bbe2c2103408962e11dd8799a35
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-18 09:38:41 +0300

    13-002 Handling Drag and Drop Events (VI)

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -8,6 +8,14 @@
       <!-- Upload Dropbox -->
       <div
         class="w-full px-10 py-20 rounded text-center cursor-pointer border border-dashed border-gray-400 text-gray-400 transition duration-500 hover:text-white hover:bg-green-400 hover:border-green-400 hover:border-solid"
+        :class="{ 'bg-green-400 border-green-400 border-solid': isDragover }"
+        @drag.prevent.stop=""
+        @dragstart.prevent.stop=""
+        @dragend.prevent.stop="isDragover = false"
+        @dragover.prevent.stop="isDragover = true"
+        @dragenter.prevent.stop="isDragover = true"
+        @dragleave.prevent.stop="isDragover = false"
+        @drop.prevent.stop="upload"
       >
         <h5>Drop your files here</h5>
       </div>
@@ -40,5 +48,17 @@
 <script>
 export default {
   name: "Upload",
+
+  data() {
+    return {
+      isDragover: false,
+    };
+  },
+
+  methods: {
+    upload() {
+      this.isDragover = false;
+    },
+  },
 };
 </script>
