27653ef6ad7625068eb79e7f12d9d6985c6c2cc8
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-23 09:16:51 +0300

    13-014 Canceling Uploads

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -118,5 +118,11 @@ export default {
       console.log(files);
     },
   },
+
+  beforeUnmount() {
+    this.uploads.forEach((upload) => {
+      upload.task.cancel();
+    });
+  },
 };
 </script>
