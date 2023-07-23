da50320279ffd2c8053859fb08d53749cee08f68
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-07-23 09:28:28 +0300

    13-015 Cancelling Uploads with Refs (vi) [pay attention the last minute]

--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -114,15 +114,19 @@ export default {
           }
         );
       });
+    },
 
-      console.log(files);
+    cancelUploads() {
+      this.uploads.forEach((upload) => {
+        upload.task.cancel();
+      });
     },
   },
 
-  beforeUnmount() {
-    this.uploads.forEach((upload) => {
-      upload.task.cancel();
-    });
-  },
+  // beforeUnmount() {
+  //   this.uploads.forEach((upload) => {
+  //     upload.task.cancel();
+  //   });
+  // },
 };
 </script>
