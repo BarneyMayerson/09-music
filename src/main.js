6b52ffb7385ea1a7e76a002808bd232d2ffece88
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-08-01 15:58:13 +0300

    17-005 Offline Support with Service Workers (vi)

--- src/main.js
+++ src/main.js
@@ -10,8 +10,12 @@ import VeeValidatePlugin from "./includes/validation";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
+import { registerSW } from "virtual:pwa-register";
+
+registerSW({ immediate: true });
 
 let app;
+
 auth.onAuthStateChanged(() => {
   if (!app) {
     app = createApp(App);
