f7dfdd371d70f5ece5d069befcd3f835ad81e3e6
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-08-02 13:21:49 +0300

    18-005 Progress Bar (VI) [using side library with vue]

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m5qp409fkoo"
+    "revision": "0.1t9a6u4akf8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
