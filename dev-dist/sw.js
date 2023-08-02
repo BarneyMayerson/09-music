3cfe0642f835577c056cf0df1dffcdb7d474ce75
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-08-02 16:14:32 +0300

    18-006 Code Coverage (must see)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1t9a6u4akf8"
+    "revision": "0.r1j56rmojt"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
