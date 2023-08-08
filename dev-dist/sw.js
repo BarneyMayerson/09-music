356324337469fea7e5b6ac7b3cb0c647ed3b6c8d
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-08-08 14:24:38 +0300

    Updating snapshots

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5357ef54'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.khhih1kc19"
+    "revision": "0.p671dj36t"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
